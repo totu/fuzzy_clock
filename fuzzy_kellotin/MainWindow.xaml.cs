9e4f0a6013549fba832c6c811fbea32876c4ca61
totu <topi.tuulensuu@gmail.com>
2015-06-06 22:49:14 +0300

    Removed unccessary dependencies and separated location to a variable

--- fuzzy_kellotin/MainWindow.xaml.cs
+++ fuzzy_kellotin/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace fuzzy_kellotin
 {
@@ -19,12 +8,13 @@ namespace fuzzy_kellotin
   {
     private bool black = true;
     private string APIKEY = "";
+    private string LOCALE = "Riihimäki,fi";
     
     public MainWindow()
     {
       InitializeComponent();
       Clock clock = new Clock(kello);
-      Weather weather = new Weather(lampotila, APIKEY);
+      Weather weather = new Weather(lampotila, LOCALE, APIKEY);
       clock.start();
       weather.start();
     }
--- fuzzy_kellotin/weather.cs
+++ fuzzy_kellotin/weather.cs
@@ -9,13 +9,15 @@ namespace fuzzy_kellotin
   class Weather
   {
     private string APIKEY;
+    private string LOCALE;
     private Label weather;
     private DispatcherTimer timer;
 
-    public Weather(Label w, string key)
+    public Weather(Label w, string locale, string apikey)
     {
       weather = w;
-      APIKEY = key;
+      APIKEY = apikey;
+      LOCALE = locale;
       timer = new DispatcherTimer();
       timer.Tick += timerTick;
       timer.Interval = new TimeSpan(0, 30, 0);
@@ -42,7 +44,7 @@ namespace fuzzy_kellotin
       string tempeture = "API error";
       using (var webClient = new System.Net.WebClient())
       {
-        var js = new WebClient().DownloadString("http://api.openweathermap.org/data/2.5/weather?q=Riihim%C3%A4ki,fi&APIID=" + APIKEY);
+        var js = new WebClient().DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + LOCALE + "&APIID=" + APIKEY);
         dynamic json = JsonConvert.DeserializeObject(js);
         tempeture = Convert.ToString(Math.Round((Convert.ToDouble(json.main.temp) - 273.15))) + " °C";
       }
