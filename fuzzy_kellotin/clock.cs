87d86e8be35e6fb866d3aac8c0aeaebf60c0c25a
totu <topi.tuulensuu@gmail.com>
2015-06-08 13:49:36 +0300

    Added smooth fading for clock and changed time check from every 1 seconds to every minute

--- fuzzy_kellotin/clock.cs
+++ fuzzy_kellotin/clock.cs
@@ -8,18 +8,23 @@ namespace fuzzy_kellotin
   class Clock
   {
     private Label clock;
-    private DispatcherTimer timer;
+    private DispatcherTimer timer = new DispatcherTimer();
+    private DispatcherTimer drawer = new DispatcherTimer();
+    private bool showing = true;
+    private double c = 1.0;
     private Fuzzier fuzz;
     private int hours;
     private int minutes;
+    private string previous = null;
 
     public Clock(Label c)
     {
       clock = c;
-      timer = new DispatcherTimer();
       timer.Tick += timerTick;
-      timer.Interval = new TimeSpan(0, 0, 1);
+      timer.Interval = new TimeSpan(0, 1, 0);
       fuzz = new Fuzzier();
+      drawer.Tick += drawTick;
+      drawer.Interval = new TimeSpan(0, 0, 0, 0, 50);
     }
 
     public void start()
@@ -33,12 +38,41 @@ namespace fuzzy_kellotin
       getTime();
     }
 
+    private void drawTick(object sender, EventArgs e)
+    {
+      if (showing == false)
+      {
+        c = c + 0.1;
+        if (c >= 1)
+        {
+          showing = true;
+          drawer.Stop();
+        }
+      }
+      else
+      {
+        c = c - 0.1;
+        if (c <= 0)
+        {
+          showing = false;
+          clock.Content = previous;
+        }
+      }
+      clock.Opacity = c;
+    }
+
     private void getTime()
     {
       DateTime time = DateTime.Now;
       minutes = time.Minute;
       hours = time.Hour;
-      clock.Content = fuzz.FuzzyUp(hours, minutes);
+      string fuzzyTime = fuzz.FuzzyUp(hours, minutes);
+
+      if (previous == null || fuzzyTime != previous)
+      {
+        previous = fuzzyTime;
+        drawer.Start();
+      }
     }
   }
 }
